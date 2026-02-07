ce064523acc2e09863858cdc77b5504e01c82130
Vitaliy Yarovuy <vitaliy.yxz@gmail.com>
2026-02-07 18:46:33 +0200

    Try fix mobile point detection problem

--- src/index.js
+++ src/index.js
@@ -72,7 +72,7 @@ map.addControl(new DotsControl({
     appliedMarkers = [];
     latLngs = [];
 
-    mgrsInput = mgrsInput.replaceAll(/[\s]+/g, ' ');
+    mgrsInput = mgrsInput.normalize("NFKC").replaceAll(/[\s]+/g, ' ');
     // Normalize Cyrillic characters to Latin
     const normalizedInput = normalizeCyrillicToLatin(mgrsInput);
     let markerCount = 0;
